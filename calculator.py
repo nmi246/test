c33382c0ce293407ff93378773b72151d58d0be8
nmi246 <nazirmik@gmail.com>
2022-07-17 21:57:11 +1200

    added nRemainer and nFloorDivide function in calculator.py. Added more function usages in main.py

--- calculator.py
+++ calculator.py
@@ -13,7 +13,17 @@ def nSubtract(x, y):
 
 # Multiply function
 def nMultiply(x, y):
-    return x - y
+    return x * y
 
 
 # Divide function
+def nDivide(x, y):
+    return x / y
+
+# remainder functions
+def nRemainder(x, y):
+    return x % y
+
+# floor divide function
+def nFloorDivide(x, y):
+    return x // y
--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import calculator as calc
 a = 10
 b = 3
 
-print("a+b: {}".format(calc.nAdd(a, b)))
+print("a + b: {}".format(calc.nAdd(a, b)))
 
+print("a - b: {}".format(calc.nSubtract(a, b)))
 
-print("a-b: {}".format(calc.nSubtract(a, b)))
+print("a * b: {}".format(calc.nMultiply(a, b)))
+
+print("a / b: {}".format(calc.nDivide(a, b)))
+
+print("a % b: {}".format(calc.nRemainder(a, b)))
+
+print("a // b: {}".format(calc.nFloorDivide(a, b)))
+
 
 # pauses program
 input()
