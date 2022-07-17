dc5592639ffe64f94112dcae69f44496e23877b3
nmi246 <nazirmik@gmail.com>
2022-07-17 21:41:30 +1200

    added calculator.py and main.py files

---
+++ calculator.py
@@ -0,0 +1,19 @@
+# this file contains common calculator functions:
+
+
+def nAdd(x, y):
+    # Add function
+    return x + y
+
+
+# subtract function
+def nSubtract(x, y):
+    return x - y
+
+
+# Multiply function
+def nMultiply(x, y):
+    return x - y
+
+
+# Divide function
---
+++ main.py
@@ -0,0 +1,12 @@
+import calculator as calc
+
+a = 10
+b = 3
+
+print("a+b: {}".format(calc.nAdd(a, b)))
+
+
+print("a-b: {}".format(calc.nSubtract(a, b)))
+
+# pauses program
+input()
